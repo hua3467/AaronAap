f0a4e17fc602a1c5104cf9c45441c4e883441a7b
Aaron Yang <zhenhuayang@outlook.com>
2021-02-04 14:21:50 -0600

    update

---
+++ js/Project.js
@@ -0,0 +1,11 @@
+let Project = function(name, type, year, location, city, state, description, link, imageUrl) {
+    this.name = name,
+    this.type = type,
+    this.year = year,
+    this.location = location,
+    this.city = city,
+    this.state = state,
+    this.description = description,
+    this.link = link,
+    this.imageUrl = imageUrl
+}
